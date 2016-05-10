b917752129cb3b789e5b8369d8e1a6343c1c6105
Anis Kadri <anis@apache.org>
2016-05-09 18:08:33 -0700

    initial commit. setting up cordova in existing android project

---
+++ android/ComponentCase/app/src/main/java/phonegap/com/componentcase/MainActivity.java
@@ -0,0 +1,13 @@
+package phonegap.com.componentcase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
