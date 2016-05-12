cf9bce6db7466590b4d3db5b421334b284dee7f7
EddyVerbruggen <eddyverbruggen@gmail.com>
2016-05-12 11:20:24 +0200

    Step 2: Adding Cordova via CocoaPods

---
+++ ios/step2-using-cocoapods-to-add-cordova/PGDayEU16/PGDayEU16/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PGDayEU16
+//
+//  Created by Eddy Verbruggen on 12/05/16.
+//  Copyright © 2016 Eddy Verbruggen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
