27c50acc5f2d86f1463d14e6aa7233b7b94028f9
EddyVerbruggen <eddyverbruggen@gmail.com>
2016-05-13 20:45:46 +0200

    Added step 5

---
+++ ios/step5-custom-plugin-for-web-native-communication/PGDayEU16/PGDayEU16/CordovaListEditorViewController.m
@@ -0,0 +1,42 @@
+//
+//  CordovaListEditor.m
+//  PGDayEU16
+//
+//  Created by Eddy Verbruggen on 13/05/16.
+//  Copyright © 2016 Eddy Verbruggen. All rights reserved.
+//
+
+#import "CordovaListEditor.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+  CGRect viewBounds = self.view.bounds;
+  CGFloat tabBarHeight = self.tabBarController.tabBar.frame.size.height;
+  CGRect webViewBounds = CGRectMake(viewBounds.origin.x,
+                                    viewBounds.origin.y,
+                                    viewBounds.size.width,
+                                    viewBounds.size.height - tabBarHeight);
+
+  self.webView.frame = webViewBounds;
+  self.webView.backgroundColor = [UIColor clearColor];
+
+  [super viewWillAppear:animated];
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
